723d1717314eeaefd036a3e4211019449278e503
nathaiml852 <nathaiml852@gmail.com>
2025-04-18 16:29:50 +0530

    updates

--- build/lib/heartfailure_model/__init__.py
+++ build/lib/heartfailure_model/__init__.py
@@ -1,9 +1,9 @@
-# __init__.py
 import sys
 from pathlib import Path
 file = Path(__file__).resolve()
 parent, root = file.parent, file.parents[1]
 sys.path.append(str(root))
+
 
 from heartfailure_model.config.core import PACKAGE_ROOT, config
 with open(PACKAGE_ROOT / "VERSION") as version_file:
